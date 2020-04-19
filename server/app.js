f43e013c8663ef8d2148a2e64fe76844fb819e41
NearlyBatman <denskyldige@hotmail.com>
2020-04-19 17:42:19 +0200

    mongoDb change

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.static('../client/build'));
 let Question;
 (async _=> {
     try{
-        let url = process.env.MONGODB_URL || 'mongodb+srv://new-user__31:123321@cluster0-ou0ly.mongodb.net/meh';
+        let url = 'mongodb+srv://new-user__31:123321@cluster0-ou0ly.mongodb.net/meh';
         await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
     }
     catch(e){
